968bfd792f9089c9e9c3a84afb46386f6d71a1d7
Amit Ganatra <amit.ganatra@moat.com>
2017-06-18 15:32:49 +0000

    removing unneeded files

--- src/deparse.h
+++
@@ -1,6 +0,0 @@
-#ifndef SQLITE_FDW_DEPARSE_H
-#define SQLITE_FDW_DEPARSE_H
-
-
-
-#endif
--- src/funcs.h
+++
@@ -1,7 +0,0 @@
-#ifndef SQLITE_FDW_FUNCS_H
-#define SQLITE_FDW_FUNCS_H
-
-#include "sqlite_fdw.h"
-
-
-#endif
